c69ddd6c1f8a4dbb65c2a4284789a2b425c0f76e
Margaret Sit <margaret.sit@sap.com>
2019-08-26 11:28:59 -0700

    clean up decorators code for logger in shell

--- metacli/decorators.py
+++ metacli/decorators.py
@@ -72,20 +72,11 @@ def loadLogging(func=None, *, logger_name="metacli"):
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-
-        ###########
-
         logger = get_logger(logger_name)
         setattr(func, "logger", logger)
-
-        ##############
         try:
             return func.main(*args, **kwargs)
         except Exception:
-            ###############
-            #logger = get_logger(logger_name)
-            ###############
-
             logger.exception("Exception occurred in CatchAllExceptions()")
             logger.error(stackprinter.format())
             file_location = logger.handlers[0].baseFilename
